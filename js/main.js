f9fb2d09a5e3deec80cc1758d9d10ef3bd5f8da8
Jamsloth <jam.sloth.se@gmail.com>
2020-09-22 16:20:29 +0900

    add js and add function

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function add() {
+
+  var screenName = document.getElementById('screenNameId').value;
+  var screenExplanation = document.getElementById('screenExplanationId').value;
+  document.getElementById('outputScreenName').innerHTML = screenName;
+  document.getElementById('outputScreenExplanation').innerHTML = screenExplanation;
+
+};
